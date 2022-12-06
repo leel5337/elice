181f845f1d1627960ffa5624ac1817e5b2a3a571
이정현 <5337myung@gmail.com>
2022-12-06 22:32:55 +0900

    유저설정

---
+++ Chapter01/12-02/02/answer.sh
@@ -0,0 +1,10 @@
+#유저설정
+
+#지시사항
+# 1. git config --global user.name명령어로 이름을 설정해 봅시다.
+# 2. git config --global user.email명령어로 이메일을 설정해 봅시다.
+# 3. git config --list명령어를 사용해 이름과 이메일이 설정한 대로 출력되는지 확인해보세요.
+
+git config --global user.name
+git config --global user.email
+git config --list
