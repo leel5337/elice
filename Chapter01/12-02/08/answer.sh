4ce0f1190bf926ab762540448099a2f22252238b
이정현 <5337myung@gmail.com>
2022-12-07 21:18:25 +0900

    브랜치 병합하기

---
+++ Chapter01/12-02/08/answer.sh
@@ -0,0 +1,10 @@
+#브랜치 병합하기
+
+#지시사항
+# 1. 현재 브랜치를 확인해보고 master브랜치로 이동해 봅시다.
+# 2. git log를 이용해 커밋 기록을 확인해 봅시다! 총 몇개의 커밋이 있나요?
+# 3. git merge add_app을 이용해 add_app브랜치와 병합해 봅시다. 아래와 같이 결과에서 Fast-forward가 있으면 성공입니다.
+# 4. git log를 이용해 다시 커밋 기록을 확인해 봅시다! add_app의 커밋 기록까지 잘 병합되었나요?
+
+git checkout master
+git merge add_app
