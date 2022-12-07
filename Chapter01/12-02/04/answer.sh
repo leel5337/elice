c6c2a17504b3586e00226e3480bb435ebc3e22d1
이정현 <5337myung@gmail.com>
2022-12-07 21:07:14 +0900

    저장소 만들고 커밋하기

---
+++ Chapter01/12-02/04/answer.sh
@@ -0,0 +1,13 @@
+#저장소 만들고 커밋하기
+
+#지시사항
+# 1. git add명령어를 이용해서 main.py파일을 준비영역에 추가합니다.
+# 2. git status를 이용해 main.py가 준비영역에 잘 추가되었는지 확인해 봅시다.
+# 3. git commit -m명령어를 이용해서 준비영역에 추가된 파일을 커밋해 봅시다.
+# 4. 마지막으로 git log라고 입력해 봅시다. 커밋한 기록이 남은게 보이시나요?
+
+git init
+
+git add main.py
+
+git commit -m "Initial Commit"
