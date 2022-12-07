af1ea02c2af3283ea3fd0fe9b4fa5ac2238600b4
이정현 <5337myung@gmail.com>
2022-12-07 21:16:16 +0900

    브랜치 생성하기

---
+++ Chapter01/12-02/07/answer.sh
@@ -0,0 +1,14 @@
+#브랜치 생성하기
+
+#지시사항
+# 1. git branch를 통해 현재 어떤 브랜치가 있는지 확인해 봅시다.
+# 2. git branch elice명령어를 이용해 elice브랜치를 만들어 봅시다.
+# 3. 다시 git branch명령어를 이용해 브랜치가 잘 만들어졌는지 확인해 봅시다. 아래와 같이 출력되면 성공입니다.
+# 4. git checkout elice명령어로 elice로 브랜치를 이동해 봅시다.
+# 5. 다시 git branch명령어를 이용해 현재 브랜치가 바뀌었는지 확인해 봅시다. 아래와 같이 출력되면 성공입니다.
+
+git branch
+git branch elice
+git branch
+git checkout elice
+git branchq
