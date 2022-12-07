6e89c958f01d48751b16b33d062d8882a740c20f
이정현 <5337myung@gmail.com>
2022-12-07 21:57:07 +0900

    여러 줄로 출력하기

---
+++ Chapter02/12-05/02/index.js
@@ -0,0 +1,12 @@
+// 여러 줄로 출력하기
+
+// 지시사항
+//  1. 400을 출력한 후 document.write를 한번 더 사용해 문자열 “<br>”을 웹 화면에 출력하세요.
+//  2. “bus” 문자열에 “<br>” 문자열을 더해서 출력하세요.
+//  3. “like” 문자열을 “li<br>ke”로 수정해서 출력하세요.
+
+document.write(400,"<br>")
+
+document.write("bus","<br>")
+
+document.write("li","<br>","ke")
