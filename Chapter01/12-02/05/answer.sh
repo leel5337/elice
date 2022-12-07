47726048d042e03e3a2680a5ca9f7412b22e7803
이정현 <5337myung@gmail.com>
2022-12-07 21:09:09 +0900

    현재 상황을 확인해보기

---
+++ Chapter01/12-02/05/answer.sh
@@ -0,0 +1,10 @@
+#현재 상황을 확인해보기
+
+#지시사항
+# 1. 가장 먼저 git status명령어를 실행해 보세요.
+# 2. 어떤 결과가 나오는지 확인해보세요!
+# 3. 앞서 배운 add 명령어를 이용해 crawling.py파일만 준비영역에 넣어보세요.
+# 4. 다시 git status명령어를 사용하면 어떻게 출력되나요?
+
+git status
+git add crawling.py
