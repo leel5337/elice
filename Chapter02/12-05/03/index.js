5859a93b8f60b264cd9d3e251ae792eec738991b
이정현 <5337myung@gmail.com>
2022-12-07 21:59:07 +0900

    따옴표 출력하기

---
+++ Chapter02/12-05/03/index.js
@@ -0,0 +1,9 @@
+// 따옴표 출력하기
+
+// 지시사항
+//  1. document.write()를 사용해 큰따옴표와 작은따옴표 없이 Its all right.를 출력합니다.
+//  2. 큰따옴표로 문자열을 감싸고 It's all right.를 출력합니다.
+//  3. 작은따옴표로 문자열을 감싸고 "Its all right."를 출력합니다.
+//  4. document.write()를 사용해 큰따옴표와 작은따옴표 앞에 \를 붙여서 "It's all right."를 출력합니다.
+
+document.write("\"It\'s all right.\"")
