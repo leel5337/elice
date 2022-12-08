e7c2da2010a4d3517b161c494d1cd06dffc06327
이정현 <5337myung@gmail.com>
2022-12-08 22:19:48 +0900

    데이터 타입 - 문자열

---
+++ Chapter02/12-06/02/index.js
@@ -0,0 +1,12 @@
+// 데이터 타입 - 문자열
+
+// 지시사항
+//  1. var를 사용하여 문자열 변수 str1을 선언하고, 데이터 “Hello World” 을 삽입합니다.
+//  2. var를 사용하여 문자열 변수 str2을 선언하고, 데이터 ‘Nice to meet you’ 을 삽입합니다.
+//  3. var를 사용하여 문자열 변수 str3을 선언하고, 데이터 “She’s a girl” 을 삽입합니다.
+//  4. document.write(); 또는 document.writeln();를 사용하여 세 변수들을 출력합니다.
+
+var str1 = "Hello World";
+var str2 = "Nice to meet you";
+var str3 = "She's a girl";
+document.write(str1, str2, str3)
