f2ade64320e32c8babc5fc8f69897eec18f25645
이정현 <5337myung@gmail.com>
2022-12-08 22:52:12 +0900

    프로퍼티와 메서드 - 문자를 숫자로 변환하는 메서드

---
+++ Chapter02/12-06/12/index.js
@@ -0,0 +1,11 @@
+// 프로퍼티와 메서드 - 문자를 숫자로 변환하는 메서드
+
+// 지시사항
+//  1. 변수 str1에 문자열 데이터 “20.14” 를 입력합니다.
+//  2. parseInt()를 사용하여 str1을 정수형 데이터 타입으로 변환시킵니다.
+//  3. parseFloat()를 사용하여 str1을 실수형 데이터 타입으로 변환시킵니다.
+//  4. 위 변수를 document.write로 출력하여 확인해봅시다.
+
+var str1 = "20.14";
+document.write(parseInt(str1));
+document.write(parseFloat(str1));
