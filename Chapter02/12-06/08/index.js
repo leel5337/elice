f404646428b7a04b704e60b492a74da5daa47e9f
이정현 <5337myung@gmail.com>
2022-12-08 22:39:02 +0900

    데이터 타입 - Boolean

---
+++ Chapter02/12-06/08/index.js
@@ -0,0 +1,11 @@
+// 데이터 타입 - Boolean
+
+// 지시사항
+//  1. true 값을 갖는 변수 t를 선언합니다.
+//  2. false 값을 갖는 변수 f를 선언합니다.
+//  3. 두 변수를 document.write()로 출력하여 데이터를 확인합니다.
+
+var t = true;
+var f = false;
+document.write(t);
+document.write(f);
