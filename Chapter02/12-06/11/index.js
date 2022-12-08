9dbdc9d08be104d8d21dd7df6cf5a3eb5d73437a
이정현 <5337myung@gmail.com>
2022-12-08 22:49:19 +0900

    프로퍼티와 메서드 - Math

---
+++ Chapter02/12-06/11/index.js
@@ -0,0 +1,12 @@
+// 프로퍼티와 메서드 - Math
+
+// 지시사항
+// abs를 사용하여 -3의 절댓값을 구하고 document.write()로 출력하세요.
+// ceil를 사용하여 0.3의 올림 값을 구하고 document.write()로 출력하세요.
+// floor를 사용하여 10.9의 내림 값을 구하고 document.write()로 출력하세요.
+// random을 사용하여 임의의 숫자를 구하고 document.write()로 출력하세요.
+
+document.write(Math.abs(-3));
+document.write(Math.ceil(0.3));
+document.write(Math.floor(10.9));
+document.write(Math.random());
