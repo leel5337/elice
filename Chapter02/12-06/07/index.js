068f3822b9849f50ece7e206bcfd4c243cd03c40
이정현 <5337myung@gmail.com>
2022-12-08 22:36:32 +0900

    데이터 타입 - undefined, null

---
+++ Chapter02/12-06/07/index.js
@@ -0,0 +1,10 @@
+// 데이터 타입 - undefined, null
+
+// 지시사항
+//  1. 아무 데이터도 넣지 않은 변수 str1을 생성하고, document.write()로 어떻게 출력되는지 확인합니다.
+//  2. 데이터 null값을 넣은 변수 empty를 생성하고, document.write()로 어떻게 출력되는지 확인합니다.
+
+var str1;
+document.write(str1);
+var empty = null;
+document.write(empty);
