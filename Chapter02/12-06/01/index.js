56ad7989511d054ecbe19ebc82903f0dffb741ad
이정현 <5337myung@gmail.com>
2022-12-08 22:14:32 +0900

    변수 생성

---
+++ Chapter02/12-06/01/index.js
@@ -0,0 +1,17 @@
+// 변수 생성
+
+// 지시사항
+//  1. var를 사용하여 변수 fruit을 선언하고, 데이터 apple을 삽입합니다.
+//  2. var를 사용하여 변수 box를 선언하고, 데이터 banana를 삽입합니다.
+//  3. document.write();을 사용하여 변수 fruit과 box의 데이터를 확인합니다.
+//  4. 변수 box의 데이터를 tomato로 변경합니다.
+//  5. document.write();을 사용하여 변수 box의 변경된 데이터를 확인합니다.
+
+var fruit = "apple";
+var box = "banana";
+
+document.write(fruit);
+document.write(box);
+
+var box = "tomato";
+document.write(box);
