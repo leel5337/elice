6fcbf63d59a9f5f95d93587c78ccb33f9079d092
이정현 <5337myung@gmail.com>
2022-12-08 22:43:05 +0900

    프로퍼티와 메서드  - 문자열

---
+++ Chapter02/12-06/09/index.js
@@ -0,0 +1,13 @@
+// 프로퍼티와 메서드 - 문자열
+
+// 지시사항
+//  1. 문자열 “Hello World” 를 데이터로 갖는 변수 str1을 선언합니다.
+//  2. .length를 사용하여 변수의 길이를 확인합니다.
+//  3. .charAt을 사용하여 0번째 데이터를 확인합니다.
+//  4. .split을 사용하여 공백" "을 기준으로 문자열을 나눕니다.
+//  5. 위 변수들을 document.write()로 출력하여 데이터를 확인합니다.
+
+var str1 = "Hello World";
+document.write(str1.length);
+document.write(str1.charAt(0));
+document.write(str1.split(" "));
