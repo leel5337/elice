15096dcacdf90e1f70e0fbab6607dcc143a1ebd9
이정현 <5337myung@gmail.com>
2022-12-08 22:23:06 +0900

    데이터 타입 - 숫자

---
+++ Chapter02/12-06/03/index.js
@@ -0,0 +1,10 @@
+// 데이터 타입 - 숫자
+
+// 지시사항
+//  1. var를 사용하여 숫자 변수 num1을 생성하고, 데이터 10을 삽입합니다.
+//  2. var를 사용하여 숫자 변수 num2를 생성하고, 데이터 3.14을 삽입합니다.
+//  3. document.write(num1 + num2) 를 입력해 num1과 num2를 더한 값을 알아봅니다.
+
+var num1 = 10;
+var num2 = 3.14;
+document.write(num1 + num2)
