4214f3db4a674aa2da7ea4d5e7a2635bbe804052
이정현 <5337myung@gmail.com>
2022-12-12 20:45:19 +0900

    산술 연산자

---
+++ Chapter02/12-08/01/index.js
@@ -0,0 +1,20 @@
+// 산술 연산자
+
+// 지시사항
+//  1. 숫자 데이터 타입인 20과 10에 산술 연산자 + - * / %를 적용합니다.
+//  2. 문자열 데이터 타입인 “20” 과 “10” 에 산술 연산자 + - * / %를 적용합니다.
+//  3. 문자열 데이터 타입인 “Elice(공백)” 과 “Lee” 에 산술 연산자 +를 사용합니다.
+
+document.write(20 + 10);
+document.write(20 - 10);
+document.write(20 * 10);
+document.write(20 / 10);
+document.write(20 % 10);
+
+document.write("20" + "10");
+document.write("20" - "10");
+document.write("20" * "10");
+document.write("20" / "10");
+document.write("20" % "10");
+
+document.write("Elice " + "Lee");
