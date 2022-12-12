2bcdb07586d35bcb45afa43945fe1cef5bae23ea
이정현 <5337myung@gmail.com>
2022-12-12 20:48:28 +0900

    비교 연산자

---
+++ Chapter02/12-08/03/index.js
@@ -0,0 +1,20 @@
+// 비교 연산자
+
+// 지시사항
+//  1. 10과 10을 ==과 ===을 사용해 비교해 document.writeln()로 출력해보세요.
+//  2. 10과 “10” 을 ==과 ===을 사용해 비교해 document.writeln()로 출력해보세요.
+//  3. 10과 20을 !==을 사용해 비교해 document.writeln()로 출력해보세요.
+//  4. 10과 20을 네 가지 부등호(>, >=, <, <=)를 사용해 비교해 document.writeln()로 출력해보세요.
+
+document.write(10 == 10);
+document.write(10 === 10);
+
+document.write(10 == "10");
+document.write(10 === "10");
+
+document.writeln(10 !== 20);
+
+document.writeln(10 > 20);
+document.writeln(10 >= 20);
+document.writeln(10 < 20);
+document.writeln(10 <= 20);
