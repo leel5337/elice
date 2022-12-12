5739b64c86c24b741aa6502b28605b83f5fa5587
이정현 <5337myung@gmail.com>
2022-12-12 21:05:13 +0900

    조건문 - do ~ while문

---
+++ Chapter02/12-08/10/index.js
@@ -0,0 +1,14 @@
+// 조건문 - do ~ while문
+
+// 지시사항
+//  1. var i = 12;를 입력하여 변수를 생성합니다.
+//  2. 조건문 형식 do { } while ( );을 입력합니다.
+//  3. do에서 {수행할 명령}으로 document.write(i);와 i++;을 입력합니다.
+//  4. while의 (조건)으로 i<10을 입력합니다.
+
+var i = 12;
+
+do {
+    document.write(i);
+    i++;
+} while (i < 10);
