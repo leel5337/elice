1f6c8ad1883409b05764277d2615ddeaf9fbd285
이정현 <5337myung@gmail.com>
2022-12-12 21:03:36 +0900

    반복문. - while문

---
+++ Chapter02/12-08/09/index.js
@@ -0,0 +1,12 @@
+// 반복문 - while문
+
+// 지시사항
+//  1. var num = 0;을 입력하여 변수를 생성합니다.
+//  2. num이 10보다 작은 경우, document.write()로 출력 후 num++ 연산을 합니다.
+
+var num = 0;
+
+while (num < 10) {
+    document.write(num);
+    num++;
+}
