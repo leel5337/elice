9ed839a31cc3c779741effb95deb69bb96dd0b3d
이정현 <5337myung@gmail.com>
2022-12-12 20:54:18 +0900

    조건문 - if ~ else문

---
+++ Chapter02/12-08/06/index.js
@@ -0,0 +1,14 @@
+// 조건문 - if ~ else문
+
+// 지시사항
+//  1. 조건이 false인 경우에 수행할 명령document.write("a > b");를 입력합니다.
+//  2. 조건이 false가 되도록 변수 a와 b의 값을 바꿔봅시다.
+
+var a = 40;
+var b = 20;
+
+if ( a < b ) {
+    document.write("a < b");
+} else {
+    document.write("a > b");
+}
