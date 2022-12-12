aa6d6c1da534fdbf3aaff3342e09bde259443751
이정현 <5337myung@gmail.com>
2022-12-12 20:46:58 +0900

    증감 연산자

---
+++ Chapter02/12-08/02/index.js
@@ -0,0 +1,17 @@
+// 증감 연산자
+
+// 지시사항
+//  1. 숫자 20을 데이터로 갖는 변수 num을 선언합니다.
+//  2. ++num 값을 출력해 봅니다.
+//  3. --num 값을 출력해 봅니다.
+//  4. num++과 num값을 출력해 봅니다.
+//  5. num--과 num값을 출력해 봅니다.
+
+var num = 20;
+
+document.write(++num);
+document.write(--num);
+document.write(num++);
+document.write(num);
+document.write(num--);
+document.write(num);
