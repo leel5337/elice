579430edc9f8fbf8a13173c2c83e0fff475eb242
이정현 <5337myung@gmail.com>
2022-12-18 17:27:53 +0900

    화면의 상단으로 이동하기

---
+++ Chapter03/12-12/02/index.js
@@ -0,0 +1,15 @@
+// 화면의 상단으로 이동하기
+
+// 지시사항
+// 화면 우측 하단의 버튼을 클릭했을 때 화면의 최상단으로 이동하도록 하는 주어진 함수 scrollUp()을 구현해봅시다.
+//     1.  함수를 적용할 변수 target을 지정하세요.
+//     2.  버튼 클릭 시 화면의 최상단으로 이동하도록 addEventListener 메서드를 적용하세요.
+
+function scrollUp(e) {
+    const target = document.querySelector(`#${e}`);
+
+    console.log(target);
+    target.addEventListener("click", () => {window.scrollTo({top: 0, left: 0, behavior: "smooth"})});
+}
+
+scrollUp("scroll-btn");
